bee7d98c2170eafc7b94701e03067b20ade51eae
LilHeizy <vishnyakov98@gmail.com>
2025-02-06 23:29:48 +0100

    cors configured; localhost allowed

--- django_blueprint/django_blueprint/settings.py
+++ django_blueprint/django_blueprint/settings.py
@@ -46,9 +46,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework_simplejwt",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -149,3 +151,7 @@ SIMPLE_JWT = {
 
 
 AUTH_USER_MODEL = "main.User"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
